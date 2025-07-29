4464bc384144e5e79729c7a5acb9e8e701f84287
migayalach <ayalachavezmiguel@gmail.com>
2025-07-28 22:48:30 -0400

    created fake data

---
+++ src/mocks/index.ts
@@ -0,0 +1,17 @@
+import { ItemToList } from "@/types";
+
+export const myHomeworks: ItemToList[] = [
+  { id: "1One1", description: "Hello world", state: false },
+  { id: "2two2", description: "My name is Miguel", state: false },
+  {
+    id: "3three3",
+    description: "I'm learning English",
+    state: true,
+  },
+  {
+    id: "4four4",
+    description: "Tomorrow I don't go to class",
+    state: false,
+  },
+  { id: "5five5", description: "I need money", state: false },
+];
