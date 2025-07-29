9fcb8e387b47379fe36f1cf4879ee3c3800244d8
migayalach <ayalachavezmiguel@gmail.com>
2025-07-28 23:53:45 -0400

    import and export new components

--- src/components/intex.ts
+++ src/components/intex.ts
@@ -3,5 +3,19 @@ import InputTask from "./inputTask/InputTask";
 import TableList from "./table/TableList";
 import ButtonAction from "./buttons/buttonAction/ButtonAction";
 import ButtonLogin from "./buttons/buttonLogin/ButtonLogin";
+import ButtonSignUp from "./buttons/buttonSignUp/ButtonSignUp";
+import CheckBoxAction from "./checkBoxAction/CheckBoxAction";
+import Message from "./message/Message";
+import NavBar from "./navBar/NavBar";
 
-export { List, InputTask, TableList, ButtonAction, ButtonLogin };
+export {
+  List,
+  InputTask,
+  TableList,
+  ButtonAction,
+  ButtonLogin,
+  ButtonSignUp,
+  CheckBoxAction,
+  Message,
+  NavBar,
+};
