63abb26e600fb6dc2248bbf680cad7d8fb3075e2
migayalach <ayalachavezmiguel@gmail.com>
2025-07-28 22:49:34 -0400

    index file

---
+++ src/components/intex.ts
@@ -0,0 +1,7 @@
+import List from "./list/List";
+import InputTask from "./inputTask/InputTask";
+import TableList from "./table/TableList";
+import ButtonAction from "./buttons/buttonAction/ButtonAction";
+import ButtonLogin from "./buttons/buttonLogin/ButtonLogin";
+
+export { List, InputTask, TableList, ButtonAction, ButtonLogin };
