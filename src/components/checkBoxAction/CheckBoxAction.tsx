abcb7899a4abdb3a7e3b9dcbf0585fb2f7eb9049
migayalach <ayalachavezmiguel@gmail.com>
2025-07-29 23:54:11 -0400

    added logic and React.memo

--- src/components/checkBoxAction/CheckBoxAction.tsx
+++ src/components/checkBoxAction/CheckBoxAction.tsx
@@ -1,19 +1,38 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Checkbox } from "antd";
+import { Checkbox, notification } from "antd";
 import type { CheckboxProps } from "antd";
+import type { NotificationPlacement } from "antd/es/notification/interface";
 
 interface InputCheckBox {
   id: string;
   state: boolean;
+  action: (id: string, state: boolean) => void;
 }
 
-function CheckBoxAction({ id, state }: InputCheckBox) {
+type NotificationType = "success" | "info" | "warning" | "error";
+
+function CheckBoxAction({ id, state, action }: InputCheckBox) {
   const [value, setValue] = useState(false);
+  const [api, contextHolder] = notification.useNotification();
 
   const onChange: CheckboxProps["onChange"] = (e) => {
     setValue(e.target.checked);
+    action(id, !value);
     // TODO UPDATE THE STATE IN THE DATA BASE WHEN I'M GOINT TO SENT THE ID AND NEW STATE
+  };
+
+  const openNotificationWithIcon = (
+    type: NotificationType,
+    placement: NotificationPlacement = "topRight"
+  ) => {
+    api[type]({
+      message: "Good job Pepe",
+      description:
+        "Congratulations, you have done your homework, let's go for more.",
+      placement,
+      duration: 2,
+    });
   };
 
   useEffect(() => {
@@ -23,7 +42,18 @@ function CheckBoxAction({ id, state }: InputCheckBox) {
     };
   }, [state]);
 
-  return <Checkbox onChange={onChange} checked={value} />;
+  return (
+    <div>
+      {contextHolder}
+      <Checkbox
+        onChange={onChange}
+        checked={value}
+        onClick={() =>
+          !value && openNotificationWithIcon("success", "bottomRight")
+        }
+      />
+    </div>
+  );
 }
 
-export default CheckBoxAction;
+export default React.memo(CheckBoxAction);
