c395136b999c3c42644a7d56916d5ebfcead976c
migayalach <ayalachavezmiguel@gmail.com>
2025-07-30 02:08:03 -0400

    created new components

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import InputTask from "../inputTask/InputTask";
+import { Calendary } from "../intex";
 
 function List() {
   return (
     <div>
       <h1>My to-do list today</h1>
+      <Calendary />
       <InputTask />
     </div>
   );
