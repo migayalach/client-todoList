d025276f5f844ed1af58cce53c92da4f5d7f8a85
migayalach <ayalachavezmiguel@gmail.com>
2025-07-30 23:56:36 -0400

    created new component

--- src/components/buttons/buttonLogin/ButtonLogin.tsx
+++ src/components/buttons/buttonLogin/ButtonLogin.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { Button, Modal } from "antd";
 
 function ButtonLogin() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div>ButtonLogin</div>
-  )
+    <div className="ml-5">
+      <Button type="primary" onClick={showModal}>
+        Open Modal
+      </Button>
+      <Modal
+        title="Basic Modal"
+        closable={{ "aria-label": "Custom Close Button" }}
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+    </div>
+  );
 }
 
-export default ButtonLogin
+export default ButtonLogin;
