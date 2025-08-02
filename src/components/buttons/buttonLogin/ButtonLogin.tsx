d2b650795add81ec2716b65cc34749de2f6ac72d
migayalach <ayalachavezmiguel@gmail.com>
2025-08-01 23:15:01 -0400

    added new case to sign out

--- src/components/buttons/buttonLogin/ButtonLogin.tsx
+++ src/components/buttons/buttonLogin/ButtonLogin.tsx
@@ -2,9 +2,11 @@
 import React, { useState } from "react";
 import { Button, Modal } from "antd";
 import { Form } from "@/components/intex";
+import { useAuth } from "@/context/authContext";
 
 function ButtonLogin() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const { user, login, logout } = useAuth();
 
   const showModal = () => {
     setIsModalOpen(true);
@@ -18,20 +20,40 @@ function ButtonLogin() {
     setIsModalOpen(false);
   };
 
+  const onChangeLogOut = () => {
+    logout();
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="ml-5">
       <Button type="primary" onClick={showModal}>
-        Sign In
+        {!login && !user?.email?.length ? "Sign In" : "Sign out"}
       </Button>
       <Modal
-        title="Welcome"
+        title={`${
+          !login && !user?.email?.length
+            ? "Welcome"
+            : "Are you leaving so soon?"
+        }`}
         closable={{ "aria-label": "Custom Close Button" }}
         open={isModalOpen}
         onOk={handleOk}
         onCancel={handleCancel}
         footer={null}
       >
-        <Form />
+        {!login && !user?.email?.length && <Form />}
+        {login && user?.email && (
+          <div>
+            <h1>{user?.displayName} are you sure you want to log out?</h1>
+            <Button type="primary" onClick={onChangeLogOut}>
+              Yes
+            </Button>
+            <Button type="primary" onClick={handleCancel}>
+              No
+            </Button>
+          </div>
+        )}
       </Modal>
     </div>
   );
