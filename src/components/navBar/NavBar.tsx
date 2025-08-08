3360b57dbb17c49504657b4f2df818b4a8adabec
migayalach <ayalachavezmiguel@gmail.com>
2025-08-08 13:35:57 -0400

    added styles

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,18 +1,19 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { ButtonLogin, MyMenu } from "../index";
 import Link from "next/link";
 import { useAuth } from "@/context/authContext";
 
 function NavBar() {
   const { user, login } = useAuth();
+  const [nav, setNav] = useState();
 
   return (
-    <div className="flex justify-between items-center">
-      <div>
+    <div className="flex md:justify-between items-center text-white w-full p-0">
+      <div className="w-[50%]">
         <Link href="./">LOGO</Link>
       </div>
-      <div className="flex">
+      <div className="w-[50%] flex justify-around items-center">
         <Link href="./">Home</Link>
         <Link href="/instructions">Instructions</Link>
         {!user?.email?.length && !login ? <ButtonLogin /> : <MyMenu />}
